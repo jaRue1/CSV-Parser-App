06bd8eef044a9cd55e3af4613f5ee4f929e3ee06
jaRue1 <jaruejohnson@outlook.com>
2021-08-07 20:55:04 -0400

    added a menu and more styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
-import React, {Component} from 'react'
+import React, { Component } from "react"
 import TableContainer from "./anime-table/TableContainer.js"
+import Table from "./anime-table/Table.js"
+import MenuBar from "./navbar/MenuBar.js"
+import history from "../history.js"
+import { Router, Route, Switch } from "react-router-dom"
 
-export default class App extends Component { // react class component
+export default class App extends Component {
+  // react class component
   render() {
-    return(
-      <TableContainer/>
+    return (
+      <Router history={history}>
+        <MenuBar />
+        <Switch>
+          <Route path="/home" exact component={TableContainer} />
+          <Route path="/test" exact component={Table} />
+          <Route />
+        </Switch>
+      </Router>
     )
   }
 }
--- src/components/anime-table/ResetButton.js
+++ src/components/anime-table/ResetButton.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const ResetButton = ({onClick}) => {
+const ResetButton = ({onClick}) => { // this a react component that will reset the sql DB
   return(
   <i className="reset-button"
     onClick={onClick}
