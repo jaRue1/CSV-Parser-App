f44e953dd020066d1daa4a47525fae6b13fefa3f
jaRue1 <jaruejohnson@outlook.com>
2022-07-22 00:08:29 -0400

    formatted table contaner

--- src/components/anime-table/TableContainer.js
+++ src/components/anime-table/TableContainer.js
@@ -1,12 +1,13 @@
-import React, {Component} from 'react'
-import ResetButton from './ResetButton.js'
+import React, { Component } from "react"
+import ResetButton from "./ResetButton.js"
 import Table from "./Table.js"
 import KH from "../../res/gifs/KH.gif"
 import AnimeStore from "../../stores/AnimeStore"
 import Actions from "../../actions"
-export default class TableContainer extends Component { // react class component
+export default class TableContainer extends Component {
+  // react class component
   state = {
-    animes: [] // empty array 
+    animes: [], // empty array
   }
 
   componentDidMount() {
@@ -16,17 +17,17 @@ export default class TableContainer extends Component { // react class component
   componentWillUnmount() {
     AnimeStore.unlisten(this.onChange)
   }
-  onChange = store => {
+  onChange = (store) => {
     const { animes } = store
-    this.setState({animes})
+    this.setState({ animes })
   }
 
-  handleDelete = event =>{
+  handleDelete = (event) => {
     const { id } = event.target
     Actions.deleteAnime(id)
   }
 
-  handleReset = () =>{
+  handleReset = () => {
     Actions.resetAnimeTable("")
     Actions.resetAnimeGenreTable("")
     Actions.resetUserTable("")
@@ -36,29 +37,37 @@ export default class TableContainer extends Component { // react class component
   }
 
   render() {
-    
-    if(!this.state.animes){
-      return null;
+    if (!this.state.animes) {
+      return null
     }
-      const data = this.state.animes.map(anime => {
-        anime.Actions = <i className ="fa fa-trash-alt delete-button" id = {anime.anime_id} onClick={this.handleDelete} title="Delete"  />
-        return anime
-      })
-    return(
+    const data = this.state.animes.map((anime) => {
+      anime.Actions = (
+        <i
+          className="fa fa-trash-alt delete-button"
+          id={anime.anime_id}
+          onClick={this.handleDelete}
+          title="Delete"
+        />
+      )
+      return anime
+    })
+    return (
       <>
-      <div>
-                                   {/* minior styling */}
-        <div className="centered-row" style={{ paddingTop: 10, paddingBottom: 10 }}> 
-          <p className="table-title"> Anime Information</p>
-          <ResetButton onClick={this.handleReset} />
-        </div>
+        <div>
+          <div
+            className="centered-row"
+            style={{ paddingTop: 10, paddingBottom: 10 }}
+          >
+            <p className="table-title"> Anime Information</p>
+            <ResetButton onClick={this.handleReset} />
+          </div>
 
-        <div className="centered-row">
-          <img src={KH} alt="kingdom hearts gif"/>
+          <div className="centered-row">
+            <img src={KH} alt="kingdom hearts gif" />
+          </div>
         </div>
-
-      </div>
-      <Table data={data}/> {/* // passing the data into this component call Table */}
+        <Table data={data} />{" "}
+        {/* // passing the data into this component call Table */}
       </>
     )
   }
